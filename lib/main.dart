816783937d60d7ce925df6322650919da8892306
Marlon Savian <marlon.savian@gmail.com>
2021-09-14 22:21:32 -0300

    add ternary expression

--- lib/main.dart
+++ lib/main.dart
@@ -13,43 +13,56 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+  static const _questions = [
+    {
+      'questionText': 'What is your favorite color?',
+      'answer': ['Blue', 'Green', 'Red', 'Yellow']
+    },
+    {
+      'questionText': 'What is your favorite animal?',
+      'answer': ['Dog', 'Cat', 'Lion', 'Elephant']
+    },
+    {
+      'questionText': 'What is your favorite friend?',
+      'answer': ['John', 'Philip', 'Andrew', 'Mike']
+    },
+  ];
+
+  var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
-      questionIndex += 1;
+      _questionIndex += 1;
     });
-    print(questionIndex);
+    print(_questionIndex);
+    print(_questions.length);
+    print(_questionIndex < _questions.length);
   }
 
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': 'What is your favorite color?',
-        'answer': ['Blue', 'Green', 'Red', 'Yellow']
-      },
-      {
-        'questionText': 'What is your favorite animal?',
-        'answer': ['Dog', 'Cat', 'Lion', 'Elephant']
-      },
-      {
-        'questionText': 'What is your favorite friend?',
-        'answer': ['Jogn', 'Philip', 'Andrew', 'Mike']
-      },
-    ];
-    final currentQuestion = questions[questionIndex];
+    var _currentQuestion = _questions[0];
+
+    if (_questionIndex < _questions.length)
+      _currentQuestion = _questions[_questionIndex];
+
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('My First APP'),
-            ),
-            body: Column(
-              children: [
-                Question(currentQuestion['questionText'] as String),
-                ...(currentQuestion['answer'] as List<String>).map((answer) {
-                  return Answer(_answerQuestion, answer);
-                }).toList()
-              ],
-            )));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First APP'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Column(
+                children: [
+                  Question(_currentQuestion['questionText'] as String),
+                  ...(_currentQuestion['answer'] as List<String>).map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Text('Yeah you finish it!'),
+              ),
+      ),
+    );
   }
 }
